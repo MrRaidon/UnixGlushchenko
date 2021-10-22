9f28543ca00c46c921174c4152d4a071731075d8
Glushchenko <glushchenko_nikita@bk.ru>
2021-10-22 21:39:25 +0300

    UnixPractice1

--- UnixPractice/main.py
+++ UnixPractice/main.py
@@ -2,34 +2,40 @@
 import shutil
 import os
 from sys import platform
-
+import sys
+dirname = "forUnix"
+tecdir=os.getcwd()
 def directory_work():
     print("Для создания директрии нажмите 1" + "\n" + "Чтобы удалить директорию нажмите 2")
     path = int(input())
     if path == 1:
-        print("Введите полный путь до директории (включаю то, как вы хотите ее назвать):" + '\n')
-        name = str(input())
-        if "forUnix" not in name:
-            print("Нельзя работать вне папки forUnix")
+        print("Введите название новой директории")
+        na = str(input())
+        name = os.getcwd() +"/"+dirname + "/" + na
+        if dirname not in name:
+            print("Нельзя работать вне рабочей папки")
         else:
             os.mkdir(name)
+            os.chdir(os.getcwd()+"/"+dirname+"/"+na)
+
     if path == 2:
-        print("Введите полный путь до папки:" + '\n')
-        name = str(input())
-        if "forUnix" not in name:
-            print("Нельзя работать вне папки forUnix")
+        print("Введите название директории, которую хотите удалить:")
+        name = os.getcwd() +"/"+dirname+"/"+str(input())
+        if dirname not in name:
+            print("Нельзя работать вне рабочей папки")
         else:
             os.rmdir(name)
 
 
 def file_work():
-    print("Чтобы работать с файлом, введите полный путь до него, а потом в меню выберите действие:")
-    path = str(input())
-    if "forUnix" not in path:
-        print("Нельзя работать вне папки forUnix")
+    print("Чтобы работать с файлом, введите имя файла с расширением (если хотите работать в другой папке вводите оносительный путь), а потом в меню выберите действие:")
+    path = os.getcwd()+"/"+dirname+"/"+str(input())
+    print(path)
+    if dirname not in path:
+        print("Нельзя работать вне рабочей папки")
     else:
         print(
-            "Чтобы создать файл нажмите 1" + "\n" + "Чтобы Удалить файл нажмите 2" + "\n" + "Чтобы вывести данные из файла на экран нажмите 3" + "\n" + "Чтобы записать в файл строку нажмите 4" + "\n" + "Чтобы переименовать файл нажмите 5" + "\n" + "Чтобы скопировать файл нажмите 6")
+            "Чтобы создать файл нажмите 1" + "\n" + "Чтобы Удалить файл нажмите 2" + "\n" + "Чтобы вывести данные из файла на экран нажмите 3" + "\n" + "Чтобы записать в файл строку нажмите 4" + "\n" + "Чтобы переименовать файл нажмите 5" + "\n" + "Чтобы скопировать файл нажмите 6" +"\n"+"Чтобы переместить файл нажмите 7")
         choice = int(input())
         if choice == 1:
             content = open(path, 'a+')
@@ -47,18 +53,27 @@ def file_work():
             content.write(text)
             content.close()
         elif choice == 5:
-            print("Введите полный путь с новым названием")
-            text1 = str(input())
+            print("Введите новое название")
+            text1 = os.getcwd()+"/"+dirname+"/"+str(input())
             os.rename(path, text1)
         elif choice == 6:
-            print("Введите новый полный путь")
-            text2 = str(input())
+            print("Введите новый относительный путь (от корневой папки) с названем файла и расширением")
+            print(path,os.getcwd())
+            text2 =os.getcwd() +"/"+dirname+str(input())
             shutil.copyfile(path, text2)
+        elif choice == 7:
+            print("Введите куда переместить файл (относительный путь (от корневой папки) с названем файла и расширением)")
+            print(path,os.getcwd())
+            text2 =os.getcwd() +"/"+dirname+str(input())
+            os.replace(path, text2)
 
 
 while True:
-    print("Вы находитесь в программе 'простейший файловый менеджер' для работы создайте свою директорию 'forUnix'" + '\n'
-        "Для создания или удаления директории нажмите 1" + '\n' + "Нажмите 2 для вызова командной строки и работы вручную" + '\n' + "Для всей работы с файлами нажмите 3")
+    os.chdir(tecdir)
+    print("Вы находитесь в программе 'простейший файловый менеджер' для работы  умолчанию была создана директория 'forUnix'" + '\n'
+        "Для создания или удаления директории нажмите 1" + '\n' + "Нажмите 2 для вызова командной строки и работы вручную" + '\n' + "Для всей работы с файлами нажмите 3" + '\n' + "Чтобы переименовать рабочую директорию введите 4" + '\n' + "Для выхода нажмите 5")
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
     inp = int(input())
     if inp == 1:
         directory_work()
@@ -70,3 +85,9 @@ while True:
                 '"C:\WINDOWS\system32\cmd.exe"')  # командная строка
     elif inp == 3:
         file_work()
+    elif inp == 4:
+        print("Введите имя новой директории")
+        dirname = input()
+        os.chdir(dirname)
+    elif inp == 5:
+        sys.exit()
